624701f4527c97872524ce6225aa87bcce5a72dd
Your Name <you@example.com>
2021-06-27 16:32:01 +0500

    test

--- manage.py
+++ manage.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import locale
 import os
-import sqlite3
 import sys
-from datetime import datetime
-
-import telebot
-
+import subprocess
+import time
 
 
 def main():
@@ -26,49 +22,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-TOKEN = '1795627820:AAGqhDfebUuQUhUpJxrvmruutkW1cRBN0PI'
-CHAT_ID = '-568473273'
-
-bot = telebot.TeleBot(TOKEN, parse_mode='html')
-
-
-def read_sqlite_table():
-    try:
-        sqlite_connection = sqlite3.connect('db.sqlite3')
-        cursor = sqlite_connection.cursor()
-        sqlite_select_query = """SELECT * from atapp_news"""
-        cursor.execute(sqlite_select_query)
-        records = cursor.fetchall()
-        title = records[len(records) - 1][1]
-        content = records[len(records) - 1][2]
-        created_at = datetime.strptime(records[len(records) - 1][3], '%Y-%m-%d %H:%M:%S.%f').strftime(
-            f'%d.%m.%Y | %H:%M')
-        # src = f"D:/Personal/Programm/Python/diplom.v3/atsite/media/{records[len(records) - 1][6]}".replace('/', '\\')
-        # photo = open(src, 'rb')
-        bot.send_message(CHAT_ID, '<b>{title}</b>.\n\n'
-                                  f'{content}\n\n'
-                                  f'{os.path.abspath("db.sqlite3")}')
-        cursor.close()
-
-    except sqlite3.Error as error:
-        print("Ошибка при работе с SQLite", error)
-    finally:
-        if sqlite_connection:
-            sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
-
-
-def watch_file_update(path):
-    timestamp = os.stat(path).st_mtime
-    print(timestamp)
-    while 1:
-        if timestamp != os.stat(path).st_mtime:
-            timestamp = os.stat(path).st_mtime
-            print('Файл изменён!')
-
-            return read_sqlite_table()
-
-
-while 1:
-    watch_file_update(os.path.abspath('db.sqlite3'))
